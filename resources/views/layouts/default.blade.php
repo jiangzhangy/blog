0f81b3279a32d245202db188e359e6a62c5db998
jiangzhangy <649768017@qq.com>
2019-05-26 16:30:31 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>医美博客 - @yield('title', '主页') - 一个让自己更美的地方</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
