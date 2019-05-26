a812243455dbb26dad981b48ee0f2f2841b3847d
jiangzhangy <649768017@qq.com>
2019-05-26 17:26:27 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <title>医美博客 - @yield('title', '主页') - 一个让自己更美的地方</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">医美博客</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="" class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a href="" class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+   @yield('content')
+  </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>你美，世界更美！</h1>
+    <p class="lead">一个可以发现美的地方</p>
+    <p>分享美，我们更快乐</p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
