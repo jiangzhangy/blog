828a212c772a1259b8434824375c208158eea2e5
jiangzhangy <649768017@qq.com>
2019-05-26 17:47:44 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <img src="https://meierman-images.oss-cn-beijing.aliyuncs.com/other/part_01/logo/1557643037%281%29.jpg" alt="logo" class="brand-icon">
+  <a href="" target="_blank">拒绝害羞，自信为美</a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,18 +7,13 @@
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">医美博客</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a href="" class="nav-link" href="/help">帮助</a></li>
-        <li class="nav-item"><a href="" class="nav-link" href="#">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
 
   <div class="container">
-   @yield('content')
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 </body>
 </html>
