b5b3940a1a002f54ccc2e8bc7840f6d75d80db41
jiangzhangy <649768017@qq.com>
2019-05-26 18:09:33 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
     <p class="lead">一个可以发现美的地方</p>
     <p>分享美，我们更快乐</p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
 @stop
